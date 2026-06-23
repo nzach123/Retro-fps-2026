d34682d3a335402b22d1927cfb0ad6d4e7a3850e
nzach123 <166802610+nzach123@users.noreply.github.com>
2026-06-23 14:53:44 -0600

    Add HealthManager and DamageData components
    Introduce a simple DamageData class and a HealthManager Node3D to handle character health. HealthManager exposes max_health, gib_at and verbose, emits signals (died, gibbed, damaged, healed, health_changed), and implements hurt() and heal() logic with clamp and threshold handling. Adds corresponding .tscn and .uid files and wires the HealthManager scene into the player.tscn as a child instance.

---
+++ _src/characters/damage_data.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name DamageData
+
+var amount = 1
---
+++ _src/characters/health_manager.gd
@@ -0,0 +1,47 @@
+extends Node3D
+
+@export var max_health  = 100
+@onready var cur_health = max_health
+@export var gib_at = -10
+@export var verbose = false
+
+signal died
+signal healed
+signal damaged
+signal gibbed
+signal health_changed(cur_health, max_health)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	health_changed.emit(cur_health, max_health)
+	if verbose:
+		print("starting_health: %s%s" % [cur_health, max_health])
+
+func hurt(damage_data = DamageData):
+	if cur_health < 0:
+		return
+	cur_health -= damage_data.amount
+	if cur_health < gib_at:
+		gibbed.emit()
+	if cur_health < 0:
+		died.emit()
+	else:
+		damaged.emit()
+	health_changed.emit(cur_health, max_health)
+	if verbose:
+		print("damaged for %s, health: %s/%s" % [damage_data.amount, cur_health, max_health])
+
+func heal(amount: int):
+	if cur_health < 0 :
+		return
+	cur_health = clamp(cur_health + amount, 0, max_health)
+	healed.emit()
+	health_changed.emit(cur_health, max_health)
+	if verbose:
+		print("healed for %s, health: %s/%s" % [amount, cur_health, max_health])
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
